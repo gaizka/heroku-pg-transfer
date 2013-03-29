7895667d366f0b34ce9350129f67bce77cb8e63e
Gaizka <gaizkav@gmail.com>
2013-03-29 22:35:31 +0100

    Added option to dump only specified table(s)

--- init.rb
+++ init.rb
@@ -8,11 +8,13 @@ class Heroku::Command::Pg < Heroku::Command::Base
   #
   # -f, --from DATABASE  # source database, defaults to DATABASE_URL on the app
   # -t, --to   DATABASE  # target database, defaults to local $DATABASE_URL
+  # --table    TABLE(s)  # dump (and restore) only specified tables
   #
   def transfer
     from = options[:from] || "DATABASE"
     to   = options[:to]   || ENV["DATABASE_URL"] || env["DATABASE_URL"]
-
+    table = options[:table]
+    
     error <<-ERROR unless to
 No local DATABASE_URL detected and --to not specified.
 For information on using config vars locally, see:
@@ -32,7 +34,7 @@ https://devcenter.heroku.com/articles/config-vars#local_setup
 
     return unless confirm_command
 
-    system %{ #{pg_dump_command(from_url)} | #{pg_restore_command(to_url)} }
+    system %{ #{pg_dump_command(from_url, table)} | #{pg_restore_command(to_url)} }
   end
 
 private
@@ -53,13 +55,14 @@ private
     end
   end
 
-  def pg_dump_command(url)
+  def pg_dump_command(url, table = nil)
     uri = URI.parse(url)
     database = uri.path[1..-1]
     host = uri.host || "localhost"
     port = uri.port || "5432"
     user = uri.user ? "-U #{uri.user}" : ""
-    %{ env PGPASSWORD=#{uri.password} pg_dump --verbose -F c -h #{host} #{user} -p #{port} #{database} }
+    table_opt = table ? "-t #{table}" : ""
+    %{ env PGPASSWORD=#{uri.password} pg_dump --verbose #{table_opt} -F c -h #{host} #{user} -p #{port} #{database} }
   end
 
   def pg_restore_command(url)
